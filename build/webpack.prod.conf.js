97d89d43081ed52c9126a21d6b2a82fd778523e3
zzmhot <zzmhot@163.com>
2017-05-09 15:43:22 +0800

    'UglifyJsPlugin,插件打包时去掉警告和log'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -31,7 +31,9 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true
       },
       sourceMap: true
     }),
