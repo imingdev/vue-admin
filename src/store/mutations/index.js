571f9909a0165ee2ddbbf352dcd78d05f48ab41f
adming <hx0011920@hengxinyongli.com>
2019-04-11 11:18:13 +0800

    修复主页以及登录存储问题

--- src/store/mutations/index.js
+++ src/store/mutations/index.js
@@ -1,14 +1,14 @@
 import * as type from 'src/store/mutations/type'
-import {cookieStorage} from 'src/common/storage'
+import {sessionStorage} from 'src/common/storage'
 
 export default {
   // 设置用户信息和是否登录
   [type.SET_USER_INFO] (state, userInfo) {
-    state.userInfo = userInfo || {}
-    if (userInfo === null) {
-      cookieStorage.remove('user_info')
+    state.userInfo = userInfo
+    if (!userInfo) {
+      sessionStorage.remove('user_info')
     } else {
-      cookieStorage.set('user_info', userInfo)
+      sessionStorage.set('user_info', userInfo)
     }
   }
 }
